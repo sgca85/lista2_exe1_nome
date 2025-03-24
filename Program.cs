c5367ca3f6e5638d29b104e863e842315d0648ff
Soraia Gomes <sgca85@gmail.com>
2025-03-24 10:52:38 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+string Nome;
+string Sobrenome;
+
+Console.WriteLine("Digite seu primeiro nome");
+Nome = Console.ReadLine()!;
+Thread.Sleep(1000);
+
+Console.WriteLine("Digite seu ultimo sobrenome");
+Sobrenome = Console.ReadLine()!;
+Thread.Sleep(1000);
+
+Console.WriteLine($"Nome Completo:{Nome} {Sobrenome}");
+Thread.Sleep(1000);
+
+Console.WriteLine($"Catálogo:{Sobrenome.ToUpper()},{Nome}");
+Thread.Sleep(1000);
